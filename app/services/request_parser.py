4171155b7a12e5d899ca11d42fbd0e9c9f37b028
Sudipto Baral <sudiptobaral.me@gmail.com>
2022-09-22 16:41:19 +0600

    [#1] fix: wrong data fetched on title slug fixed.

--- app/services/request_parser.py
+++ app/services/request_parser.py
@@ -20,7 +20,7 @@ class RequestParser:
         challenge.difficulty = challenge_info.get('question').get('difficulty')
         challenge.question_id = challenge_info.get('question').get('frontendQuestionId')
         challenge.date = challenge_info.get('date')
-        challenge.title_slug = challenge_info.get('question').get('question')
+        challenge.title_slug = challenge_info.get('question').get('titleSlug')
         challenge.raw_tags = challenge_info.get('question').get('topicTags')
 
 
