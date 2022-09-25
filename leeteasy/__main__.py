b272ba8ef0548f608bc0a21313eea7396501a8d9
Sudipto Baral <sudiptobaral.me@gmail.com>
2022-09-25 14:22:44 +0600

    [#8] feature: initial command setup

--- leeteasy/__main__.py
+++ leeteasy/__main__.py
@@ -1,38 +1,25 @@
 import time as clock
-from datetime import datetime
 
 import click
 import schedule
 
-from leeteasy.services.request_handler import RequestHandler
-from leeteasy.services.request_parser import RequestParser
+from leeteasy.services.notification_service import Notifier
+from leeteasy.utils.validatiors import TimeValidator
+
+# check every 10 min
+sleep_duration = 10 * 60
 
 
 @click.command()
 @click.argument('time')
 def main(time) -> None:
     """Schedule notification at TIME."""
-    _validate_time(time)
-    schedule.every().day.at(time).do(schedule_notification)
+    TimeValidator.validate(time)
+    schedule.every().day.at(time).do(Notifier.notify)
+
     while True:
         schedule.run_pending()
-        clock.sleep(1)
-
-
-def schedule_notification():
-
-    challenge_info = RequestHandler.get_challenge_info()
-    test_challenge = RequestParser.parse(challenge_info)
-    print(test_challenge)
-
-
-def _validate_time(time: str):
-    """Validates the given string is in valid time format."""
-    time_format = '%H:%M'
-    try:
-        return datetime.strptime(time, time_format).time()
-    except ValueError:
-        click.echo('Invalid time format')
+        clock.sleep(sleep_duration)
 
 
 if __name__ == '__main__':
