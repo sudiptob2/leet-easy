84a6fb65b638e86a42cc20886772d2d8e301e528
Sudipto Baral <sudiptobaral.me@gmail.com>
2022-09-25 14:19:33 +0600

    [#8] refactor: parser updated to return challenge object

--- leeteasy/__main__.py
+++ leeteasy/__main__.py
@@ -1,6 +1,38 @@
-def main() -> None:
-    """This method is used for testing purpose."""
-    print('coming soon.')
+import time as clock
+from datetime import datetime
+
+import click
+import schedule
+
+from leeteasy.services.request_handler import RequestHandler
+from leeteasy.services.request_parser import RequestParser
+
+
+@click.command()
+@click.argument('time')
+def main(time) -> None:
+    """Schedule notification at TIME."""
+    _validate_time(time)
+    schedule.every().day.at(time).do(schedule_notification)
+    while True:
+        schedule.run_pending()
+        clock.sleep(1)
+
+
+def schedule_notification():
+
+    challenge_info = RequestHandler.get_challenge_info()
+    test_challenge = RequestParser.parse(challenge_info)
+    print(test_challenge)
+
+
+def _validate_time(time: str):
+    """Validates the given string is in valid time format."""
+    time_format = '%H:%M'
+    try:
+        return datetime.strptime(time, time_format).time()
+    except ValueError:
+        click.echo('Invalid time format')
 
 
 if __name__ == '__main__':
