00285444ac9c5137d5adc1b49d5cd207ef54939a
nilesh05apr <nk8588338@gmail.com>
2022-10-06 17:04:58 +0530

    update: test scripts

--- tests/test_services/test_request_parser.py
+++ tests/test_services/test_request_parser.py
@@ -16,7 +16,7 @@ class TestRequestParser:
             'get_challenge_info',
             fake_get_challenge_info,
         )
-        challenge = RequestParser.parse()
+        challenge = RequestParser.parse(fake_challenge_data)
 
         assert challenge.title == fake_challenge_data.get('question').get('title')
         assert challenge.difficulty == fake_challenge_data.get('question').get('difficulty')
