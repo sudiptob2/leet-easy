b805025678ee2a4a58d9785b01c41748250553e5
Sudipto Baral <sudiptobaral.me@gmail.com>
2022-10-06 13:29:09 +0600

    [#41] refactor: overused code fixed

--- leeteasy/services/request_parser.py
+++ leeteasy/services/request_parser.py
@@ -7,17 +7,18 @@ class RequestParser:
     @classmethod
     def parse(cls, challenge_info: dict) -> Challenge:
         """Parse API data ans update challenge model."""
-        return RequestParser._parse_challenge_info(challenge_info)
+        return cls._parse_challenge_info(challenge_info)
 
     @classmethod
     def _parse_challenge_info(cls, challenge_info) -> Challenge:
         """Parse and update challenge model."""
+        question = challenge_info.get('question')
         challenge = Challenge()
-        challenge.title = challenge_info.get('question').get('title')
-        challenge.ac_rate = challenge_info.get('question').get('acRate')
-        challenge.difficulty = challenge_info.get('question').get('difficulty')
-        challenge.question_id = challenge_info.get('question').get('frontendQuestionId')
+        challenge.title = question.get('title')
+        challenge.ac_rate = question.get('acRate')
+        challenge.difficulty = question.get('difficulty')
+        challenge.question_id = question.get('frontendQuestionId')
         challenge.date = challenge_info.get('date')
-        challenge.title_slug = challenge_info.get('question').get('titleSlug')
-        challenge.raw_tags = challenge_info.get('question').get('topicTags')
+        challenge.title_slug = question.get('titleSlug')
+        challenge.raw_tags = question.get('topicTags')
         return challenge
